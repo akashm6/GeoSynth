2d3fef22ec5ba27bee1be6b8a7acc858a534a58e
Akash Mohan <akhikm4@gmail.com>
2025-06-26 15:24:40 -0500

    async metadata fetch logic

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -95,44 +95,75 @@ def fetch_reports():
 
 @app.task
 def fetch_disaster_metadata():
-    url = ("https://api.reliefweb.int/v1/disasters"
-            "?appname=atlascope"
-            "&profile=full"
-            "&sort[]=date.event:desc&limit=30"
-            "&fields[include][]=country"
-            "&fields[include][]=date"
-            "&fields[include][]=profile.key_content"
-            "&fields[include][]=profile.useful_links"
-            "&fields[include][]=description"
-            "&fields[include][]=name"
-            "&fields[include][]=type"
-            "&fields[include][]=status"
-            "&fields[include][]=url_alias"
-    )
-    res = requests.get(url)
+    now = datetime.now(timezone.utc).replace(microsecond=0)
+    start = now - timedelta(days=150)
+
+    params = {
+        "appname": "atlascope",
+        "filter[conditions][0][field]": "date.created",
+        "filter[conditions][0][value][from]": start.isoformat(),
+        "filter[conditions][0][value][to]": now.isoformat(),
+        "sort[]": "date.created:desc",
+        "fields[include][]": [
+            "id", 
+            "description", 
+            "name",
+            "primary_country", 
+            "primary_type",
+            "date",
+            "status",
+            "type",
+            "profile.useful_links",
+            "url_alias"
+        ],
+        "limit": 100
+    }
+
+    base_url = "https://api.reliefweb.int/v1/disasters"
+    encoded_params = urlencode(params, doseq=True)
+    full_url = f"{base_url}?{encoded_params}"
+    res = requests.get(full_url)
+
     data = res.json()
     data = data.get("data")
+    print(data)
     results = []
     for report in data:
-        print(report)
+
         fields = report.get("fields")
-        disaster_name = fields.get("name", None)
+        disaster_id = fields.get("id", None)
+        date_event = datetime.fromisoformat(fields.get("date", {}).get("changed", None))
+        country = fields.get("primary_country", {})
+        disaster_country = country.get("shortname", None)
+        disaster_country_iso3 = country.get("iso3", None)
+        disaster_lat = country.get("location", {}).get("lat")
+        disaster_long = country.get("location", {}).get("lon")
         disaster_description = fields.get("description", None)
+        disaster_name = fields.get("name", None)
         disaster_type = fields.get("type", [])[0].get("name", None)
-        date_event = fields.get("date",{}).get("event", None)
-        country_data = fields.get("country", [])[0].get("country", {})
-        country = country_data.get("name", None)
-        country_shortname = country_data.get("shortname", None)
-        country_iso3 = country_data.get("iso3", None)
-        status = fields.get("status", None)
-        profile_data = fields.get
-
-
+        disaster_status = fields.get("status", None)
+        profile_data = fields.get("profile", {}).get("active", [])
+        profile_useful_links = [link.get("url", None) for link in profile_data.get("useful_links", {}).get("active", [])] if profile_data else None
+        disaster_url_alias = fields.get("url_alias", None)
         
+        metadata = DisasterMetaData(
+            disaster_id = disaster_id,
+            date_event = date_event,
+            disaster_country = disaster_country,
+            disaster_country_iso3 = disaster_country_iso3,
+            disaster_lat = disaster_lat,
+            disaster_long = disaster_long,
+            disaster_description = disaster_description,
+            disaster_name = disaster_name,
+            disaster_type = disaster_type,
+            disaster_status = disaster_status,
+            profile_useful_links = profile_useful_links,
+            disaster_url_alias = disaster_url_alias
+        )
 
-        print(report)
+        results.append(metadata)
 
-    return
+    return results
 
 @app.task
 def test_add(name: str):
@@ -146,4 +177,4 @@ def test_add(name: str):
         conn.commit()
         print("worked.")
 
-print(fetch_reports())
+print(fetch_disaster_metadata())
