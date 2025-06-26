7d80fab800de06584c7c13f58ca0cf96de1462a5
Akash Mohan <akhikm4@gmail.com>
2025-06-26 13:03:04 -0500

    decoupled disaster and report metadata models

--- backend/app/db_models/worldevent.py
+++ backend/app/db_models/worldevent.py
@@ -2,38 +2,46 @@ from enum import Enum
 from sqlalchemy import create_engine, text
 from typing import Optional
 from pydantic import BaseModel
+from datetime import datetime
 
 connection_string = 'postgresql+psycopg2://postgres:@localhost:5432/globedb_dev'
 engine = create_engine(connection_string)
 
-class EventType(str, Enum):
-    disaster = "disaster"
-    report = "report"
-
-class EventModel(BaseModel):
-    event_type: EventType 
-    # shared by both
-    country: str
-    country_iso3: str
-    country_shortname: Optional[str]
-    status: Optional[str]
+class ReportData(BaseModel):
+    report_id: int
+    disaster_id: Optional[int]
+    primary_country: str
+    primary_country_iso3: str
+    primary_country_shortname: Optional[str]
+    country_lat: float
+    country_long: float
     # report-specific
-    date_report_created: Optional[str] 
+    date_report_created: datetime
     headline_title: Optional[str]
     headline_image_url: Optional[str]
+    headline_image_caption: Optional[str]
     headline_summary: Optional[str]
     language: Optional[str]
     source_name: Optional[str]
     source_homepage: Optional[str]
     report_url_alias: Optional[str]
-    # disaster-specific
-    date_event: Optional[str] 
+ 
+# disaster-specific metadata for heatmaps, filters, etc.
+class DisasterMetaData(BaseModel):
+    disaster_id: int
+    date_event: datetime
     disaster_description: Optional[str]
     disaster_name: Optional[str]
+    disaster_primary_type: Optional[str]
     disaster_type: Optional[str]
+    disaster_status: Optional[str]
     profile_key_content: Optional[str] # latest situation reports
     profile_useful_links: Optional[str]
     disaster_url_alias: Optional[str]
+
+class EnrichedEvent(BaseModel):
+    report: ReportData
+    disaster_metadata: Optional[DisasterMetaData]
            
 def test_insert():
     query = """
