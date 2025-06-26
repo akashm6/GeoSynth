e03cc22b6132d6c86c329b632cd876174c7a75c5
Akash Mohan <akhikm4@gmail.com>
2025-06-26 13:24:00 -0500

    updated report fetching based on new schema

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -2,7 +2,8 @@ import requests
 from sqlalchemy import create_engine, text
 from celery import Celery
 import json
-from db_models.worldevent import EventModel, EventType
+from datetime import datetime
+from db_models.worldevent import ReportData, DisasterMetaData, EnrichedEvent
 
 app = Celery("tasks", broker = "redis://localhost:6379/0", backend="redis://localhost:6379/0")
 
@@ -16,12 +17,14 @@ def fetch_reports():
            "?appname=atlascope"
            "&filter[field]=headline"
            "&sort[]=date.created:desc&limit=100"
+           "&fields[include][]=disaster.id"
+           "&fields[include][]=id"
            "&fields[include][]=headline"
-           "&fields[include][]=country"
+           "&fields[include][]=primary_country"
            "&fields[include][]=source"
            "&fields[include][]=date"
            "&fields[include][]=language"
-           "&fields[include][]=url_alias"      
+           "&fields[include][]=url_alias"     
     )
 
     res = requests.get(url)
@@ -31,43 +34,92 @@ def fetch_reports():
     results = []
     for report in data:
         fields = report.get("fields")
-        country_data = fields.get("country", [{}])[0]
-        country = country_data.get("name")
-        if country == "World":
+        #print(fields)
+        report_id = int(fields.get("id", None))
+        disaster_data = fields.get("disaster", [])
+        disaster_id = disaster_data[0].get("id") if disaster_data else None
+        country_data = fields.get("primary_country", {})
+        primary_country = country_data.get("name")
+        if primary_country == "World":
             continue
+        primary_country_iso3 = country_data.get("iso3")
+        primary_country_shortname = country_data.get("shortname", None)
+        country_lat = country_data.get("location", {}).get("lat", None)
+        country_long = country_data.get("location", {}).get("lon", None)
+        date = fields.get("date",{}).get("created", None)
+        date_report_created = datetime.fromisoformat(date)
+        headline_data = fields.get("headline", {})
+        headline_title = headline_data.get("title", None)
+        headline_image_url = headline_data.get("image", {}).get("url-large", None)
+        headline_image_caption = headline_data.get("image", {}).get("caption", None)
+        headline_summary = headline_data.get("summary", None)
+        language = fields.get("language", [])[0].get("name", None)
+        source_name = fields.get("source", [])[0].get("shortname", None)
+        source_homepage = fields.get("source", [])[0].get("homepage", None)
+        report_url_alias = fields.get("url_alias", None)
         
-        country_iso3 = country_data.get("iso3")
-        country_shortname = country_data.get("shortname")
-        
-        event = EventModel(
-            event_type=EventType.report,
-            country=country,
-            country_iso3=country_iso3,
-            country_shortname=country_shortname,
-            status=fields.get("status", None),
-            date_report_created=fields["date"]["created"],
-            headline_title=fields.get("headline", {}).get("title"),
-            headline_image_url=fields.get("headline", {}).get("image", {}).get("url"),
-            headline_summary=fields.get("headline", {}).get("summary"),
-            language=fields.get("language", [])[0].get("name", None),
-            source_name=fields.get("source", [])[0].get("name", None),
-            source_homepage=fields.get("source", [])[0].get("homepage", None),
-            report_url_alias=fields.get("url_alias"),
-            date_event = None, 
-            disaster_description=None,
-            disaster_name=None,
-            disaster_type=None,
-            profile_key_content=None, # latest situation reports
-            profile_useful_links=None,
-            disaster_url_alias=None,
+        report = ReportData(
+            report_id = report_id,
+            disaster_id = disaster_id,
+            primary_country = primary_country,
+            primary_country_iso3 = primary_country_iso3,
+            primary_country_shortname = primary_country_shortname,
+            country_lat = country_lat,
+            country_long = country_long,
+            date_report_created = date_report_created,
+            headline_title = headline_title,
+            headline_image_url = headline_image_url,
+            headline_image_caption = headline_image_caption,
+            headline_summary = headline_summary,
+            language = language,
+            source_name = source_name,
+            source_homepage = source_homepage,
+            report_url_alias = report_url_alias
         )
 
-        results.append(event)
+        results.append(report)
 
     return results
 
 @app.task
-def fetch_disasters():
+def fetch_disaster_metadata():
+    url = ("https://api.reliefweb.int/v1/disasters"
+            "?appname=atlascope"
+            "&profile=full"
+            "&sort[]=date.event:desc&limit=30"
+            "&fields[include][]=country"
+            "&fields[include][]=date"
+            "&fields[include][]=profile.key_content"
+            "&fields[include][]=profile.useful_links"
+            "&fields[include][]=description"
+            "&fields[include][]=name"
+            "&fields[include][]=type"
+            "&fields[include][]=status"
+            "&fields[include][]=url_alias"
+    )
+    res = requests.get(url)
+    data = res.json()
+    data = data.get("data")
+    results = []
+    for report in data:
+        print(report)
+        fields = report.get("fields")
+        disaster_name = fields.get("name", None)
+        disaster_description = fields.get("description", None)
+        disaster_type = fields.get("type", [])[0].get("name", None)
+        date_event = fields.get("date",{}).get("event", None)
+        country_data = fields.get("country", [])[0].get("country", {})
+        country = country_data.get("name", None)
+        country_shortname = country_data.get("shortname", None)
+        country_iso3 = country_data.get("iso3", None)
+        status = fields.get("status", None)
+        profile_data = fields.get
+
+
+        
+
+        print(report)
+
     return
 
 @app.task
