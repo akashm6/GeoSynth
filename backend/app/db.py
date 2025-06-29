24a337a04eddab113c8b9d79bfc415376b0da63a
Akash Mohan <akhikm4@gmail.com>
2025-06-29 16:26:49 -0500

    global entryway for db engine

---
+++ backend/app/db.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+connection_string = os.getenv("DATABASE_CONN_STRING") or "postgresql+psycopg2://postgres:@localhost:5432/globedb_dev"
+engine = create_engine(connection_string)
+
+def check_conn():
+    try:
+        with engine.connect() as cursor:
+            cursor.execute("SELECT 1")
+            print("DB Connection Successful.")
+    except Exception as e:
+        print("DB Connection failed.")
+        print(e)
+
+if __name__ == "__main__":
+    check_conn()
