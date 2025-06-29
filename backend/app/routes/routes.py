bd6c11f8299d3a5f37dfb6d7da89d20a49021f5a
Akash Mohan <akhikm4@gmail.com>
2025-06-29 16:39:36 -0500

    session management + last_updated logic

--- backend/app/routes/routes.py
+++ backend/app/routes/routes.py
@@ -1,7 +1,29 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Depends
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+from db import engine, SessionLocal
+import requests
+import json
 
 router = APIRouter()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.get("/last-updated")
+def get_last_updated_time(db: Session = Depends(get_db)):
+
+    query = text("""
+    SELECT MAX(date_report_created) FROM test_reports;
+    """)
+    result = db.execute(query)
+    last_updated = result.scalar()
+    return {"last_updated": last_updated}
+        
 @router.get("/ping")
 def ping():
     return {"msg": "pong"}
