01c30987d9800ec3ad959b436a969a3f9edb88cf
Akash Mohan <akhikm4@gmail.com>
2025-07-14 19:38:04 -0500

    correctly grabs llm output and sends to frontend

--- backend/app/routes/routes.py
+++ backend/app/routes/routes.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, Depends
+from pydantic import BaseModel
 from sqlalchemy import text
 from sqlalchemy.orm import Session
-from app.db import engine, SessionLocal
+from db import engine, SessionLocal
+from llm_chain import expand_region_terms, generate
 from dotenv import load_dotenv
 from openai import OpenAI
+from db_models.worldevent import ReportData
 import requests
 import os
 import json
@@ -11,6 +14,9 @@ import json
 load_dotenv()
 
 router = APIRouter()
+
+class LLMInput(BaseModel):
+    user_input: str
 
 def get_db():
     db = SessionLocal()
@@ -29,10 +35,17 @@ def get_last_updated_time(db: Session = Depends(get_db)):
     last_updated = result.scalar()
     return {"last_updated": last_updated}
 
-@router.post("/llm-response/")
-def handle_llm_query(prompt: str):
-    return
+@router.post("/llm-response")
+def process_prompt(input: LLMInput, db:Session=Depends(get_db)):
+    user_input = input.user_input
+    response = generate(user_input)
+    sql_query = text(response.get("sql"))
+    rows = db.execute(sql_query)
+    col = rows.keys()
+    
+    result = [dict(zip(col, row)) for row in rows.fetchall()]
 
+    return {"prompt_results": result, "sql": sql_query}
         
 @router.get("/ping")
 def ping():
