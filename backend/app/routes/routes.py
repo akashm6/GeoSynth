1410a8cf10b8159148dfe80574c63fad9c188a98
Akash Mohan <akhikm4@gmail.com>
2025-07-21 18:30:45 -0500

    parses recent events and sends to frontend

--- backend/app/routes/routes.py
+++ backend/app/routes/routes.py
@@ -3,7 +3,9 @@ from pydantic import BaseModel
 from sqlalchemy import text
 from sqlalchemy.orm import Session
 from app.db import engine, SessionLocal
+from datetime import datetime, timedelta
 from app.llm_chain import expand_region_terms, generate
+from collections import defaultdict
 from dotenv import load_dotenv
 from openai import OpenAI
 from app.db_models.worldevent import ReportData
@@ -47,6 +49,42 @@ def process_prompt(input: LLMInput, db:Session=Depends(get_db)):
 
     return {"prompt_results": result, "sql": sql_query}
         
+@router.get("/grab-initial-events")
+def grab_initial_events(db: Session = Depends(get_db)):
+
+    one_week_ago = (datetime.utcnow() - timedelta(days=7)).isoformat()
+    print(one_week_ago)
+
+    query = text(f"""
+    SELECT report_id, primary_country, country_lat, country_long, date_report_created, 
+    headline_title, headline_summary, source_name, source_homepage, 
+    report_url_alias FROM test_reports 
+    WHERE date_report_created >= :one_week_ago;
+    """
+    )
+
+    rows = db.execute(query, {"one_week_ago": one_week_ago}).fetchall()
+
+    grouped = defaultdict(list)
+
+    for row in rows:
+        report = dict(row._mapping)
+        country_lat, country_long = report.get('country_lat'), report.get('country_long')
+
+        key = (country_lat, country_long)
+        grouped[key].append(report)
+        
+    result = []
+    for key, reports in grouped.items():
+        result.append({
+            "lat": key[0],
+            "long": key[1],
+            "reports": reports
+        })
+
+    return result
+    #return rows[0]
+
 @router.get("/ping")
 def ping():
     return {"msg": "pong"}
