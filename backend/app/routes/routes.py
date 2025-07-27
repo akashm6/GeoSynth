f7c30e5a51fc9f294053b8a89c1ea09fae0dc081
Akash Mohan <akhikm4@gmail.com>
2025-07-27 16:02:09 -0500

    db connect issues

--- backend/app/routes/routes.py
+++ backend/app/routes/routes.py
@@ -48,8 +48,7 @@ def get_last_updated_time(db: Session = Depends(get_db)):
         readable = last_updated.strftime("%B %d, %Y at %I:%M %p UTC")
         return readable
     except Exception as e:
-        return {"message":  e}
-        #raise HTTPException(status_code=500, detail="DB connection or query failed.")
+        return {"message":  e.name}
 
 
 @router.post("/llm-response")
