5907d4e1972a489baf81306d0d13e268db313e5d
Akash <akhikm4@gmail.com>
2025-08-27 23:42:29 -0500

    fixed ss of truth errors for redis

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -15,7 +15,8 @@ load_dotenv()
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 redis_client = redis.Redis.from_url(REDIS_URL, decode_responses=True)
 
-app = Celery("tasks", broker = redis_url, backend = redis_url)
+app = Celery("tasks", broker=REDIS_URL, backend=REDIS_URL)
+
 appname = "atlascope"
 
 '''
